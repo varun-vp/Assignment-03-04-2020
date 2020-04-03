3c90361a583f5ce5a636167d9b3b1b437c45fa3a
varun-vp <varun.pahuja96@gmail.com>
2020-04-03 15:41:10 +0530

    Final Push with Both Questions

---
+++ Question2-03-04-2020/Question2-03-04-2020/Controllers/EmpsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Question2_03_04_2020.Models;
+
+namespace Question2_03_04_2020.Controllers
+{
+    public class EmpsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Employee";
+            List<Emp> emplist = new List<Emp>()
+            {
+                new Emp{EId = 101, EName = "Sam",ESalary=30000 },
+                new Emp{EId = 102, EName = "Sohan",ESalary=35000 },
+                new Emp{EId = 103, EName = "Ram",ESalary=45550 },
+                new Emp{EId = 104, EName = "Shyam",ESalary=98000 }
+            };
+            ViewBag.EmpData = emplist;
+            return View();
+        }
+    }
+}
---
+++ Question2-03-04-2020/Question2-03-04-2020/Models/Emp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Question2_03_04_2020.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public float ESalary { get; set; }
+    }
+}
